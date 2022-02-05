081012412e8f575065c525e5c79bada425f8cef5
qu <1095698022@qq.com>
2022-02-05 23:28:37 +0800

    feat: test

--- src/pages/list/index.tsx
+++ src/pages/list/index.tsx
@@ -1,50 +1,47 @@
-import Taro from '@tarojs/taro'
-import React from 'react'
-import { View } from '@tarojs/components'
-import { ThreadList } from '../../components/thread_list'
-import { IThread } from '../../interfaces/thread'
-import api from '../../utils/api'
+import Taro from "@tarojs/taro";
+import React from "react";
+import { View } from "@tarojs/components";
+import { ThreadList } from "../../components/thread_list";
+import { IThread } from "../../interfaces/thread";
+import api from "../../utils/api";
 
-import './index.css'
+import "./index.css";
 
 interface IState {
-  loading: boolean,
-  threads: IThread[]
+  loading: boolean;
+  threads: IThread[];
 }
 
 class Hot extends React.Component<{}, IState> {
   state = {
     loading: true,
-    threads: []
-  }
+    threads: [],
+  };
 
-  async componentDidMount () {
+  async componentDidMount() {
     try {
-      const res = await Taro.request<IThread[]>({
-        url: api.getHotNodes()
-      })
-      this.setState({
-        threads: res.data,
-        loading: false
-      })
+      // const res = await Taro.request<IThread[]>({
+      //   url: api.getHotNodes(),
+      // });
+      // this.setState({
+      //   threads: res.data,
+      //   loading: false,
+      // });
     } catch (error) {
       Taro.showToast({
-        title: '载入远程数据错误'
-      })
+        title: "载入远程数据错误",
+      });
     }
   }
 
-  render () {
-    const { loading, threads } = this.state
+  render() {
+    const { loading, threads } = this.state;
     return (
-      <View className='index'>
-        <ThreadList
-          threads={threads}
-          loading={loading}
-        />
+      <View className="index">
+        <ThreadList threads={threads} loading={loading} />
       </View>
-    )
+    );
   }
 }
 
-export default Hot
+export default Hot;
