98f8720d6844cf50e1b36027f0f7e29a04d80975
quyp <1095698022@qq.com>
2022-02-08 18:41:44 +0800

    feat: index

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,6 +1,13 @@
 import Taro from "@tarojs/taro";
 import React from "react";
-import { View, Swiper, SwiperItem, Image } from "@tarojs/components";
+import {
+  View,
+  Swiper,
+  SwiperItem,
+  Image,
+  Text,
+  ScrollView,
+} from "@tarojs/components";
 import { ThreadList } from "../../components/thread_list";
 import { IThread } from "../../interfaces/thread";
 import api from "../../utils/api";
@@ -70,15 +77,42 @@ class Index extends React.Component<{}, IState> {
         });
       });
   };
+  onScrollToUpper() {}
+
+  // or 使用箭头函数
+  // onScrollToUpper = () => {}
+
+  onScroll(e) {
+    console.log(e.detail);
+  }
+
   render() {
+    const scrollStyle = {
+      height: "400px",
+    };
+    const scrollTop = 0;
+    const Threshold = 20;
+    const vStyleA = {
+      height: "150px",
+      "background-color": "rgb(26, 173, 25)",
+    };
+    const vStyleB = {
+      height: "150px",
+      "background-color": "rgb(39,130,215)",
+    };
+    const vStyleC = {
+      height: "150px",
+      "background-color": "rgb(241,241,241)",
+      color: "#333",
+    };
     // const { loading, threads } = this.state;
     return (
       <View className="index">
-        <View onClick={this.getOpenId}>test</View>
-        <View>{this.state.userinfo?.openid}</View>
+        {/* <View onClick={this.getOpenId}>test</View>
+        <View>{this.state.userinfo?.openid}</View> */}
         {/* <ThreadList threads={threads} loading={loading} /> */}
         <Swiper
-          className="test-h"
+          className="index-h"
           indicatorColor="#999"
           indicatorActiveColor="#333"
           circular
@@ -87,27 +121,56 @@ class Index extends React.Component<{}, IState> {
         >
           <SwiperItem>
             <Image
-              style="width: 300px;height: 100px;background: #fff;"
-              src="../../resource/image1.jpeg"
+              style="width:100%;height: 200px;background: #fff;"
+              src="https://alifei04.cfp.cn/creative/vcg/800/new/VCG41N1210205351.jpg"
             />
           </SwiperItem>
           <SwiperItem>
             <View className="demo-text-2">
               <Image
-                style="width: 300px;height: 100px;background: #fff;"
-                src="../../resource/image2.jpeg"
+                style="width:100%;height: 200px;background: #fff;"
+                src="https://alifei04.cfp.cn/creative/vcg/800/new/VCG41N1210205351.jpg"
               />
             </View>
           </SwiperItem>
           <SwiperItem>
             <View className="demo-text-3">
               <Image
-                style="width: 300px;height: 100px;background: #fff;"
-                src="../../resource/image3.jpeg"
+                style="width:100%;height: 200px;background: #fff;"
+                src="https://alifei04.cfp.cn/creative/vcg/800/new/VCG41N1210205351.jpg"
               />
             </View>
           </SwiperItem>
         </Swiper>
+        <View className="flex-wrp" style="flex-direction:row;">
+          <View className="flex-item">
+            <Image src="https://alifei04.cfp.cn/creative/vcg/800/new/VCG41N1210205351.jpg" />
+            <Text>123</Text>
+          </View>
+          <View className="flex-item">
+            <Image src="https://alifei04.cfp.cn/creative/vcg/800/new/VCG41N1210205351.jpg" />
+            <Text>123</Text>
+          </View>
+          <View className="flex-item">
+            <Image src="https://alifei04.cfp.cn/creative/vcg/800/new/VCG41N1210205351.jpg" />
+            <Text>123</Text>
+          </View>
+        </View>
+        <ScrollView
+          className="scrollview"
+          scrollY
+          scrollWithAnimation
+          scrollTop={scrollTop}
+          style={scrollStyle}
+          lowerThreshold={Threshold}
+          upperThreshold={Threshold}
+          onScrollToUpper={this.onScrollToUpper.bind(this)} // 使用箭头函数的时候 可以这样写 `onScrollToUpper={this.onScrollToUpper}`
+          onScroll={this.onScroll}
+        >
+          <View style={vStyleA}>A</View>
+          <View style={vStyleB}>B</View>
+          <View style={vStyleC}>C</View>
+        </ScrollView>
       </View>
     );
   }
