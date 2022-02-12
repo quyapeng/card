ec70b32d991276bd8fcc754af5b6dac9cbabb332
qu <1095698022@qq.com>
2022-02-12 22:23:03 +0800

    feat: add list

--- src/pages/list/index.tsx
+++ src/pages/list/index.tsx
@@ -1,6 +1,6 @@
 import Taro from "@tarojs/taro";
 import React from "react";
-import { View } from "@tarojs/components";
+import { View, CoverImage } from "@tarojs/components";
 import { ThreadList } from "../../components/thread_list";
 import { IThread } from "../../interfaces/thread";
 import api from "../../utils/api";
@@ -38,7 +38,18 @@ class Hot extends React.Component<{}, IState> {
     const { loading, threads } = this.state;
     return (
       <View className="index">
-        <ThreadList threads={threads} loading={loading} />
+        <View className="card">
+          <CoverImage
+            className="img"
+            src="https://alifei04.cfp.cn/creative/vcg/800/new/VCG41N1210205351.jpg"
+          />
+        </View>
+        <View className="card">
+          <CoverImage
+            className="img"
+            src="https://alifei04.cfp.cn/creative/vcg/800/new/VCG41N1210205351.jpg"
+          />
+        </View>
       </View>
     );
   }
