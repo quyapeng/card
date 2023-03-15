8964c8eee74a446c18d9621aa37ea0e62c95df01
quyp <1095698022@qq.com>
2023-03-15 18:26:38 +0800

    update

--- src/pages/list/index.tsx
+++ src/pages/list/index.tsx
@@ -1,6 +1,6 @@
 import Taro from "@tarojs/taro";
 import React from "react";
-import { View, CoverImage, Text } from "@tarojs/components";
+import { View, CoverImage, Text, Swiper, SwiperItem } from "@tarojs/components";
 // import { ThreadList } from "../../components/thread_list";
 import { IThread } from "../../interfaces/thread";
 // import api from "../../utils/api";
@@ -38,6 +38,25 @@ class List extends React.Component<{}, IState> {
     const { loading, threads } = this.state;
     return (
       <View className="index">
+        <Swiper
+          className="test-h"
+          indicatorColor="#999"
+          indicatorActiveColor="#333"
+          vertical
+          circular
+          indicatorDots
+          autoplay
+        >
+          <SwiperItem>
+            <View className="demo-text-1">1</View>
+          </SwiperItem>
+          <SwiperItem>
+            <View className="demo-text-2">2</View>
+          </SwiperItem>
+          <SwiperItem>
+            <View className="demo-text-3">3</View>
+          </SwiperItem>
+        </Swiper>
         <View
           className="card"
           onClick={() => {
