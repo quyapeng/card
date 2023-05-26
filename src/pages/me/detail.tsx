e3751c453cb404869885127bc7f33c942f135478
quyp <1095698022@qq.com>
2023-05-26 16:25:48 +0800

    update

--- src/pages/me/detail.tsx
+++ src/pages/me/detail.tsx
@@ -1,7 +1,7 @@
 import Taro from "@tarojs/taro";
 import React from "react";
 import { View, Text, Navigator, Image } from "@tarojs/components";
-import AtListItem from "/ListItem.tsx";
+import { ListItem } from "../../components/ListItem";
 
 class Details extends React.Component<{}, {}> {
   config = {
@@ -11,21 +11,21 @@ class Details extends React.Component<{}, {}> {
   render() {
     return (
       <View className="detail">
-        <AtListItem
+        <ListItem
+          url=""
           title="标题文字"
           arrow="right"
           thumb="https://img12.360buyimg.com/jdphoto/s72x72_jfs/t6160/14/2008729947/2754/7d512a86/595c3aeeNa89ddf71.png"
         />
-        <AtListItem
+        <ListItem
+          url=""
           title="标题文字"
-          note="描述信息"
           arrow="right"
           thumb="http://img10.360buyimg.com/jdphoto/s72x72_jfs/t5872/209/5240187906/2872/8fa98cd/595c3b2aN4155b931.png"
         />
-        <AtListItem
+        <ListItem
+          url=""
           title="标题文字"
-          note="描述信息"
-          extraText="详细信息"
           arrow="right"
           thumb="http://img12.360buyimg.com/jdphoto/s72x72_jfs/t10660/330/203667368/1672/801735d7/59c85643N31e68303.png"
         />
