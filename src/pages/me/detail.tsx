74eb5520864e73e8f656f60b72367f647234828e
quyp <1095698022@qq.com>
2023-05-26 17:36:11 +0800

    update: detail

--- src/pages/me/detail.tsx
+++ src/pages/me/detail.tsx
@@ -1,6 +1,6 @@
 import Taro from "@tarojs/taro";
 import React from "react";
-import { View, Text, Navigator, Image } from "@tarojs/components";
+import { View, Text, Navigator, Image, Button } from "@tarojs/components";
 import { ListItem } from "../../components/ListItem";
 
 class Details extends React.Component<{}, {}> {
@@ -13,22 +13,35 @@ class Details extends React.Component<{}, {}> {
       <View className="detail">
         <ListItem
           url=""
-          title="标题文字"
+          title="头像"
           arrow="right"
           thumb="https://img12.360buyimg.com/jdphoto/s72x72_jfs/t6160/14/2008729947/2754/7d512a86/595c3aeeNa89ddf71.png"
         />
         <ListItem
           url=""
-          title="标题文字"
+          title="昵称"
           arrow="right"
           thumb="http://img10.360buyimg.com/jdphoto/s72x72_jfs/t5872/209/5240187906/2872/8fa98cd/595c3b2aN4155b931.png"
         />
         <ListItem
           url=""
-          title="标题文字"
+          title="性别"
           arrow="right"
           thumb="http://img12.360buyimg.com/jdphoto/s72x72_jfs/t10660/330/203667368/1672/801735d7/59c85643N31e68303.png"
         />
+        <ListItem
+          url=""
+          title="城市"
+          arrow="right"
+          thumb="http://img12.360buyimg.com/jdphoto/s72x72_jfs/t10660/330/203667368/1672/801735d7/59c85643N31e68303.png"
+        />
+        <ListItem
+          url=""
+          title="生日"
+          arrow="right"
+          thumb="http://img12.360buyimg.com/jdphoto/s72x72_jfs/t10660/330/203667368/1672/801735d7/59c85643N31e68303.png"
+        />
+        <Button>保存</Button>
       </View>
     );
   }
